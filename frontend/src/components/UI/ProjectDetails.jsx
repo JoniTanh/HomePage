018829450ac148cf7a813525cf27e3d046cb8c32
Joni Tanhuansuu <jonitanhuansuu91@gmail.com>
2023-12-28 03:26:49 +0200

    Update button layout for project navigation

--- frontend/src/components/UI/ProjectDetails.jsx
+++ frontend/src/components/UI/ProjectDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import projectsData from "../../data/projectsData";
 import Carousel from "./Carousel";
 import useFetchImages from "../../hooks/useFetchImages";
@@ -7,9 +7,26 @@ import useFetchImages from "../../hooks/useFetchImages";
 export default function ProjectDetails() {
   const { projectId } = useParams();
   const [images, setImages] = useState([]);
-  const project = projectsData[projectId];
+  const navigate = useNavigate();
 
+  const projectQueries = Object.entries(projectsData)
+    .map(([key, value]) => ({
+      imageQuery: value.imageQuery,
+    }))
+    .sort((a, b) => a.imageQuery.localeCompare(b.imageQuery));
+
+  const currentProjectIndex = projectQueries.findIndex(
+    (p) => p.imageQuery === projectId
+  );
+  const project = projectsData[projectId];
   useFetchImages(project?.imageQuery, setImages);
+
+  const previousProjectIndex =
+    (currentProjectIndex - 1 + projectQueries.length) % projectQueries.length;
+  const nextProjectIndex = (currentProjectIndex + 1) % projectQueries.length;
+
+  const previousProjectQuery = projectQueries[previousProjectIndex].imageQuery;
+  const nextProjectQuery = projectQueries[nextProjectIndex].imageQuery;
 
   if (!project) {
     return <div>Project not found.</div>;
@@ -18,8 +35,32 @@ export default function ProjectDetails() {
   return (
     <>
       <div className="sourceCodePro whiteColorBlackShadow">
-        <div className="w-full xl:w-3/5 mx-auto px-10 my-20 sm:my-24">
-          <div className="flex flex-col">
+        <div className="w-full xl:w-3/5 mx-auto px-10 my-10 sm:my-12">
+          <div className="flex flex-wrap items-center justify-between mb-8">
+            <div className="mb-2 sm:mb-0">
+              <button
+                onClick={() => navigate("/portfolio")}
+                className="bg-gray-500 hover:bg-gray-700 whiteColorBlackShadow font-bold py-2 px-4 rounded mb-2"
+              >
+                Back to Portfolio
+              </button>
+            </div>
+            <div className="flex space-x-4">
+              <button
+                onClick={() => navigate(`/portfolio/${previousProjectQuery}`)}
+                className="bg-blue-500 hover:bg-blue-700 whiteColorBlackShadow font-bold py-2 px-4 rounded mb-2"
+              >
+                {"<"} Previous Project
+              </button>
+              <button
+                onClick={() => navigate(`/portfolio/${nextProjectQuery}`)}
+                className="bg-blue-500 hover:bg-blue-700 whiteColorBlackShadow font-bold py-2 px-4 rounded mb-2"
+              >
+                Next Project {">"}
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col my-10 sm:my-12">
             <div className="w-full text-justify">
               <div className="mb-4 text-2xl text-green-600">
                 {project.title}
